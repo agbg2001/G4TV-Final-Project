9587a1dfa5d1d5b7478700f4b37f17e4d7f791c5
Jettersfreak <fishes_lisa@yahoo.ca>
2022-11-17 04:36:39 -0500

    Colour swap working

--- Assets/Scripts/PlayerAnimation.cs
+++ Assets/Scripts/PlayerAnimation.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class PlayerAnimation : MonoBehaviour
 {
-    PlayerMovement playerMovement;
+    PlayerMovement playerMovement;  //movement script
+    PlayerSwap playerSwap; //swap script
     SpriteRenderer spriteRenderer;
     public Animator animator;   //reference to animator component
     public GameObject character;    //reference to player object
@@ -12,12 +13,22 @@ public class PlayerAnimation : MonoBehaviour
     void Awake()
     {
         playerMovement = character.GetComponent<PlayerMovement>();  //Access the movement script
+        playerSwap = character.GetComponent<PlayerSwap>();  //access swap script
         spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        //change sprite colour based on active colour
+        if (playerSwap.isRedActive) {
+            spriteRenderer.color = new Color(1, 0, 0, 1);   //turns sprite red
+        }
+        else {
+            spriteRenderer.color = new Color(0, 0, 1, 1);   //turns sprite blue
+        }
+
+
         //Flip sprite when moving left
         if (playerMovement.h < 0) {
             spriteRenderer.flipX = true;
@@ -25,7 +36,6 @@ public class PlayerAnimation : MonoBehaviour
         else if (playerMovement.h > 0) {
             spriteRenderer.flipX = false;
         }
-
 
         animator.SetFloat("PlayerSpeed", Mathf.Abs(playerMovement.h));   //set Speed = absolute val of horizontal movement speed
         animator.SetFloat("Vertical", playerMovement.Character.velocity.y); //get vertical speed of player

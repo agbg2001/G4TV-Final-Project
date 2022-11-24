a433454bc668f5771db23a721695b5b3dd9003ba
Angela <a28he@uwaterloo.ca>
2022-11-23 20:24:01 -0500

    Revert "pause menu merge conflict fix"
    This reverts commit 15c0503e2927c63f6aa2848cd7575a20c2a2bf75, reversing
    changes made to f4ae2b52705a582d19f05611fd891125fb3ecde1.

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -9,9 +9,5 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
-
-    public void QuitGame ()
-    {
-        Application.Quit();
-    }   
+    
 }
